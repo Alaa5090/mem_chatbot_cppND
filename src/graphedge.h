ebfbcbbdbe98145e71aa8a47250d21a766eeaccc
alaa5090 <alaaabdraboo297@gmail.com>
2022-04-07 01:21:20 +0200

    proprietary members

--- src/graphedge.h
+++ src/graphedge.h
@@ -8,4 +8,7 @@ class GraphEdge{
 private:
     GraphNode *_childNode;
     GraphNode *_parentNode;
+    int _id;
+    std::vector<std::string> _keywords; // list of topics associated with this edge
+    
 };
