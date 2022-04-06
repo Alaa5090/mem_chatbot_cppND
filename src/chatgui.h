69bce391e9b2a6c1b770f50746e28ae26241f955
alaa5090 <alaaabdraboo297@gmail.com>
2022-04-06 21:52:14 +0200

    proprietary functions

--- src/chatgui.h
+++ src/chatgui.h
@@ -6,17 +6,22 @@ class ChatLogic;
 class ChatBotPanelDialog : public wxScrolledWindow{
 private:
      wxBoxSizer *_dialogSizer;
-    wxBitmap _image;
-    std::unique_ptr<ChatLogic> _chatLogic;
+     wxBitmap _image;
+     std::unique_ptr<ChatLogic> _chatLogic;
 public:
-ChatBotPanelDialog(wxWindow *parent, wxWindowID id);
+     ChatBotPanelDialog(wxWindow *parent, wxWindowID id);
     ~ChatBotPanelDialog();
 
- ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
-void paintEvent(wxPaintEvent &evt);
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
+
+    void paintEvent(wxPaintEvent &evt);
     void paintNow();
     void render(wxDC &dc);
+    void AddDialogItem(wxString text, bool isFromUser = true);
+    void PrintChatbotResponse(std::string response);
+
+    DECLARE_EVENT_TABLE()
+};
 
 
 
-}
