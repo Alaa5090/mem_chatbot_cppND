7aa754d6b71c9ee345b5b890f926e9dd5de558c9
alaa5090 <alaaabdraboo297@gmail.com>
2022-04-07 00:47:27 +0200

    getters / setters

--- src/chatbot.h
+++ src/chatbot.h
@@ -7,11 +7,26 @@ class ChatLogic;
 class ChatBot{
     private:
      wxBitmap *_image;
-    GraphNode *_currentNode;
-    GraphNode *_rootNode;
-    ChatLogic *_chatLogic;
+       GraphNode *_currentNode;
+       GraphNode *_rootNode;
+       ChatLogic *_chatLogic;
 int ComputeLevenshteinDistance(std::string s1, std::string s2);
-   ChatBot();
- ChatBot(std::string filename);
+     public:
+       ChatBot();
+       ChatBot(std::string filename);
+      ~ChatBot();
+    ChatLogic *GetChatLogicHandle();
+
+    ChatBot(ChatBot &src);
+    ChatBot(ChatBot && src);
+
+    ChatBot &operator=(ChatBot &src);
+    ChatBot &operator=(ChatBot &&src);
+void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+   
 
 }
