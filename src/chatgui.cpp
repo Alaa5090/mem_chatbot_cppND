19c68db441fa6accd495aecdde962e7f8509750f
alaa5090 <alaaabdraboo297@gmail.com>
2022-04-08 14:34:10 +0200

    size of chatbot window

---
+++ src/chatgui.cpp
@@ -0,0 +1,10 @@
+#include <wx/filename.h>
+#include <wx/colour.h>
+#include <wx/image.h>
+#include <string>
+#include "chatgui.h"
+#include "chatbot.h"
+#include "chatlogic.h"
+
+const int width = 414;
+const int height = 736;
