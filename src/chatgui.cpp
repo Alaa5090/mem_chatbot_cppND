803305e537d5f48d507ed9f0d55c87ef1790dd0c
alaa5090 <alaaabdraboo297@gmail.com>
2022-04-08 14:51:09 +0200

    position window in screen center

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -26,14 +26,18 @@ bool ChatBotApp::OnInit()
 ChatBotFrame::ChatBotFrame(const wxString &title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(width, height))
 {
     ChatBotFrameImagePanel *ctrlPanel = new ChatBotFrameImagePanel(this);
+   
    _panelDialog = new ChatBotPanelDialog(ctrlPanel, wxID_ANY);
+    
     int idTextXtrl = 1;
     _userTextCtrl = new wxTextCtrl(ctrlPanel, idTextXtrl, "", wxDefaultPosition, wxSize(width, 50), wxTE_PROCESS_ENTER, wxDefaultValidator, wxTextCtrlNameStr);
     Connect(idTextXtrl, wxEVT_TEXT_ENTER, wxCommandEventHandler(ChatBotFrame::OnEnter));
+     
      wxBoxSizer *vertBoxSizer = new wxBoxSizer(wxVERTICAL);
     vertBoxSizer->AddSpacer(90);
     vertBoxSizer->Add(_panelDialog, 6, wxEXPAND | wxALL, 0);
     vertBoxSizer->Add(_userTextCtrl, 1, wxEXPAND | wxALL, 5);
     ctrlPanel->SetSizer(vertBoxSizer);
 
+     this->Centre();
 }
